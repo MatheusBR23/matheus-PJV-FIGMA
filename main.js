d54e6e1e262b773080617f05847262c1ddaf157b
MatheusBR23 <145366291+MatheusBR23@users.noreply.github.com>
2025-04-16 08:03:44 -0300

    atualizado

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-6-08T30:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = new Date("2026-6-08T30:00:00") - tempoAtual;
 
